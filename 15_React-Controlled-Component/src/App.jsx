6bf23ba69a51dd4e225269d830fb1858829c8997
Sahil-khola <blackiayaduvanshia@gmail.com>
2024-12-15 22:13:33 +0530

    REACT

---
+++ 15_React-Controlled-Component/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+function App() {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  return (
+    <>
+      <h1>Controlled Component</h1>
+      <form action="" method="get">
+        <input type="text"value={name} onChange={(event)=>{setName(event.target.value)}} placeholder="Enter Name" />
+        <br />
+        <br />
+        <input type="password"value={password} onChange={(event)=>{setPassword(event.target.value)}}  placeholder="Enter password" />
+        <br />
+        <br />
+        <input type="text" value={email} onChange={(event)=>{setEmail(event.target.value)}} placeholder="Enter email" />
+        <br />
+        <br />
+        <button>Submit</button>
+        <button onClick={()=>{setName("");setPassword("");setEmail("")}} > Clear </button>
+        <h1>{name}</h1>
+        <h1>{password}</h1>
+        <h1>{email}</h1>
+      </form>
+    </>
+  );
+}
+
+export default App;
