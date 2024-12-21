9cda207ba94b98338a17d251baa03c4368b0f1a7
Sahil-khola <blackiayaduvanshia@gmail.com>
2024-12-21 20:41:15 +0530

    REACT

--- 25_React-Style/src/App.jsx
+++ 25_React-Style/src/App.jsx
@@ -1,10 +1,19 @@
-
 function App() {
-
   return (
-  <>
-  </>
-  )
+    <>
+      <div style={
+        { color: "red",
+        fontSize: "20px",
+        backgroundColor: "black",
+        display:'inline-block',
+        height:'200px',
+        width:'200px',
+        textAlign:'center',
+        }}>
+        Hello Khola
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
