9ae8f092d059d9d274d10f0e673ebead90f81516
Sahil-khola <blackiayaduvanshia@gmail.com>
2024-12-24 21:28:24 +0530

    REACT

--- 27_React-External-Style/src/App.jsx
+++ 27_React-External-Style/src/App.jsx
@@ -1,10 +1,38 @@
 
-
 function App() {
 
   return (
     <>
-
+     <h1 className="heading" >External Style </h1>
+    <div className="card-container">
+    <div className="card">
+      <div>
+      <img className="img-Style" src="https://picsum.photos/200" alt="random" />
+      </div>
+      <div className='text-wrap'>
+        <h4>Sahil khola</h4>
+        <p>Software developer</p>
+      </div>
+     </div>
+     <div className="card">
+      <div>
+      <img className="img-Style" src="https://picsum.photos/200" alt="random" />
+      </div>
+      <div className='text-wrap'>
+        <h4>Sahil khola</h4>
+        <p>Software developer</p>
+      </div>
+     </div>
+     <div className="card">
+      <div>
+      <img className="img-Style" src="https://picsum.photos/200" alt="random" />
+      </div>
+      <div className='text-wrap'>
+        <h4>Sahil khola</h4>
+        <p>Software developer</p>
+      </div>
+     </div>
+    </div>
     </>
   )
 }
