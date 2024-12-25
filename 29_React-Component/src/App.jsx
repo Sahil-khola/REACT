52193b759a2971ec448b8c3f7ef022b6c17ffcf0
Sahil-khola <blackiayaduvanshia@gmail.com>
2024-12-25 13:28:54 +0530

    REACT

--- 29_React-Component/src/App.jsx
+++ 29_React-Component/src/App.jsx
@@ -1,10 +1,28 @@
-function App() {
+ import styled from 'styled-components'
+ function App() {
+ 
+  // const Heading= styled.h2`
+  // color:red;
+  // border:2px solid red;
+  // border-radius:5px;
+  // margin:20px;
+  // padding:20px
+  // `
 
+  const Heading=styled.h1({
+    color:red,
+    border:"2px solid red",
+    borderRadius:"5px",
+    margin:"20px",
+    padding:"20px",
+  })
   return (
     <>
-     
+     <h1>Style Component</h1>
+     <Heading>hello heading</Heading>
     </>
   )
 }
 
 export default App
+h
