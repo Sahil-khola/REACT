5ac7ea01460258e95bb935cd11fc1d5958ebe239
Sahil-khola <blackiayaduvanshia@gmail.com>
2024-12-27 14:32:07 +0530

    REACT

--- 32_React-Uncontrolled-Component/src/App.jsx
+++ 32_React-Uncontrolled-Component/src/App.jsx
@@ -1,13 +1,47 @@
-
+import { useRef } from "react";
 
 function App() {
-
+  function submitForm(event) {
+    event.preventDefault();
+    const user = document.querySelector("#user").value;
+    console.log(user);
+    const password = document.querySelector("#pass").value;
+   console.log(password);
+  }
+  function submitFormRef(event) {
+    event.preventDefault();
+    const user = userRef.current.value
+    const password = passwordRef.current.value
+    console.log(user,password);
+    
+  }
+  const userRef = useRef();
+  const passwordRef = useRef();
   return (
     <>
-      
-
+      <h1>Uncontrolled Component</h1>
+      <form action="" method="post" onSubmit={submitForm}>
+        <input type="text" placeholder="Enter Name" />
+        <br />
+        <br />
+        <input type="password" placeholder="Enter Password " />
+        <br />
+        <br />
+        <button>Submit </button>
+       </form>
+       <hr />
+      <h1>Uncontrolled Component with useRef</h1>
+      <form action="" method="post" onSubmit={submitFormRef}>
+        <input type="text" placeholder="Enter Name" ref={userRef} id="user" />
+        <br />
+        <br />
+        <input type="password" placeholder="Enter Password " ref={passwordRef} id="pass" />
+        <br />
+        <br />
+        <button>Submit Ref</button>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
