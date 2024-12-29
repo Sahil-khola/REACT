4defba0e7f49465147bf2323fbac311a4a3f883e
Sahil-khola <blackiayaduvanshia@gmail.com>
2024-12-29 13:46:42 +0530

    REACT

---
+++ 35_React-FormStatus-Hook/src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+ async function handelSubmit() {
+  await new Promise((res => setTimeout(res, 2000)));
+  console.log("Form Submitted");
+ }
+  return (
+    <div>
+      <form onSubmit={handelSubmit}>
+        <input type="text" placeholder="username" />
+        <br /><br />
+        <input type="password" placeholder="password"/>
+        <br /><br />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+} 
+export default App;
