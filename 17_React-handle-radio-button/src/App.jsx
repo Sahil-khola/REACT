25825648f3970a86998befc0d6854e9acf83d085
Sahil-khola <blackiayaduvanshia@gmail.com>
2025-01-04 13:59:40 +0530

    REACT

--- 17_React-handle-radio-button/src/App.jsx
+++ 17_React-handle-radio-button/src/App.jsx
@@ -1,42 +1,36 @@
 import { useState } from "react";
 
 function App() {
-  const [gender,setGender] = useState("");
-  function handelRadio(event) {
-    setGender(event.target.value)
-  }
-  
-  const [city,setCity]=useState("Gurugram");
-  function handelCity(event) {
-    setCity(event.target.value)
+  const [gender, setGender] = useState("Female");
+  function chnageRadio(event) {
+    setGender(event.target.value);
   }
 
+
+  const [city, setCity] = useState("gurugram");
+  function chnageCity(event){
+    setCity(event.target.value);
+  }
   return (
-    <>
-      <h1>Handel Radio and Dropdown </h1>
-      <form action="" method="get">
-        <h3>Select Gender</h3>
-        <input type="radio" name="gender" id="male" onChange={(event)=>{event.target.value}} value={'Male'} 
-        checked ={gender=='Male'}/>
-        <label htmlFor="male">Male</label>
-        <br />
-        <br />
-        <input type="radio" name="gender" id="female" onChange={handelRadio} value={'Female'}
-          checked ={gender=='Female'} />
-        <label htmlFor="female">Female</label>
-        <h2>Selected Gender: {gender}</h2>
-      </form>
-      <br /><br /><br /><br />
-      
-        <h4>Select City</h4>
-        <select defaultValue={"gurugram"}>
-          <option value="noida" onChange={handelCity}>Noida</option>
-          <option value="gurugram">Gurugram</option>
-          <option value="delhi">Delhi</option>
-        </select>
-      <h1>Selected City : {city}</h1>
-      
-    </>
+    <div>
+      <h1>Handel Radio and Dropdown</h1>
+      <h3> Select Gender</h3>
+      <input type="radio" name="Gender" id="Male"  value='Male'checked={gender=='Male'} onChange={chnageRadio}/>
+      <label htmlFor="Male">MALE</label>
+      <input type="radio" name="Gender" id="Female" value='female' checked={gender=='female'} onChange={chnageRadio} />
+      <label htmlFor="Female">FEMALE</label>
+      <h4>Selected Gender:{gender}</h4>
+      <br /><br /><br />
+     <hr />
+     <hr />
+      <h1>Select city</h1>
+      <select name="City" defaultValue={"gurugram"} onChange={chnageCity}>
+        <option value="noida" >Noida</option>
+        <option value="gurugram">GURUGRAM</option>
+        <option value="delhi">DELHI</option>
+      </select>
+      <h3>selected city :{city}</h3>
+    </div>
   );
 }
 export default App;
